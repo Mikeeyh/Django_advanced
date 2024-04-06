5925b3d28d02fcf83409ea55d777249a4cef87f5
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2024-04-07 02:50:33 +0300

    add stripe

--- git_deployment_demos/git_deployment_demos/settings.py
+++ git_deployment_demos/git_deployment_demos/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uiomf@c*0op5bh&cy^^!(od5qv+f#-ii1l-d8czwfscg7qehci'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", 0)
 
 ALLOWED_HOSTS = [
     '192.168.1.6',
